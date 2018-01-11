dca360ab52bc94c64bd078b7e4f5fbcb15efb8c7
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-11 17:32:05 -0200

    Added ReplaySystem and Touchscreen Joystick

---
+++ Twin Sticks 2.5D/Assets/Player/ReplaySystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+	private Rigidbody rigidBody;
+	private GameManager manager;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody>();
+		manager = FindObjectOfType<GameManager>();
+	}
+
+	void Update() {
+		if ( manager.recording ) {
+			Record();
+		} else if (Time.frameCount > bufferFrames) {
+			Playback();
+		} else {
+			Record();
+		}
+	}
+
+	void Playback() {
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		transform.position = keyFrames[frame].position;
+		transform.rotation = keyFrames[frame].rotation;
+	}
+
+	void Record(){
+		rigidBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+       	float time = Time.time;
+       	keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+	}
+}
+
+/// A structure for storing time, positon and rotation.
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame(float time, Vector3 pos, Quaternion rot) {
+		this.frameTime = time;
+		this.position = pos;
+		this.rotation = rot;
+	}
+}
