c9fbb6dc8f6cf5954753c09f9ef17ebcd10e4c77
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-12 09:07:28 -0200

    Pausing Your Game

--- Twin Sticks 2.5D/Assets/GAME/GameManager.cs
+++ Twin Sticks 2.5D/Assets/GAME/GameManager.cs
@@ -6,6 +6,12 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
 	public bool recording = true;
+	private bool isPaused = false;
+	private float initialFixedDeltaTime;
+
+	void Start() {
+		initialFixedDeltaTime = Time.fixedDeltaTime;
+	}
 
 	void Update () {
 		if ( CrossPlatformInputManager.GetButton("Fire1") ){
@@ -13,5 +19,25 @@ public class GameManager : MonoBehaviour {
 		} else {
 			recording = true;
 		}
+
+		if ( Input.GetKeyDown( KeyCode.P ) ) {
+			if ( isPaused ) { isPaused = false; ResumeGame(); }
+			else { isPaused = true; PauseGame(); }
+		}
+
+	}
+
+	void PauseGame() {
+		Time.timeScale = 0f;
+		Time.fixedDeltaTime = 0f;
+	}
+
+	void ResumeGame() {
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = initialFixedDeltaTime;
+	}
+
+	void OnApplicationPause( bool pauseStatus ) {
+		isPaused = pauseStatus;
 	}
 }
