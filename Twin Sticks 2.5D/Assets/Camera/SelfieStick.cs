e6807880e809fc1f4b99f01dd99c1f8d819869ab
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-13 17:44:43 -0200

    Reading From 2nd Stick

---
+++ Twin Sticks 2.5D/Assets/Camera/SelfieStick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieStick : MonoBehaviour {
+
+	public float panSpeed = 2f;
+
+	private GameObject player;
+	private Vector3 armRotation;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	void Update () {
+		armRotation.y -= Input.GetAxis("RHoriz") * panSpeed;
+		armRotation.z += Input.GetAxis("RVert") * panSpeed;
+
+		transform.position = player.transform.position;
+		transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
