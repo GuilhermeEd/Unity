eb10279a947399fded3504e0c051a259e0ed994a
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-18 21:40:07 -0200

    MultiPlayer Cameras

--- Ahoy Matey/Assets/Players/Player.cs
+++ Ahoy Matey/Assets/Players/Player.cs
@@ -6,10 +6,13 @@ using UnityEngine.Networking;
 
 public class Player : NetworkBehaviour {
 
+	public float mouseSensitivity = 3f;
+
 	private Vector3 inputValue;
+	private Camera cam;
+	private float mouseX, mouseY;
 
 	void Update () {
-
 		if(!isLocalPlayer) return;
 
 		inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
@@ -17,5 +20,18 @@ public class Player : NetworkBehaviour {
 		inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
 		
 		transform.Translate(inputValue);
+		
+		mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
+		mouseY += Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+		cam.transform.eulerAngles = new Vector3(-mouseY, mouseX ,0f);
 	}
+
+	public override void OnStartLocalPlayer(){
+		if(isLocalPlayer){
+			cam = GetComponentInChildren<Camera>();
+			cam.enabled = true;
+		}
+	}
+	
 }
