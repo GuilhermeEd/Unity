29c04e944971d63a972b9a62b774d44de27268d9
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-21 00:03:48 -0200

    Using Time.deltaTime

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	public float boosterPower = 20f;
+	public float rotationSensitivity = 80f;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		ProcessInput();
+	}
+
+	void ProcessInput () {
+		if ( Input.GetKey ( KeyCode.Space ) ) {
+			rb.AddRelativeForce( Vector3.up * boosterPower );
+		}
+
+		if ( Input.GetKey ( KeyCode.A ) ) {
+			transform.Rotate( Vector3.forward * rotationSensitivity * Time.deltaTime );
+		} else if ( Input.GetKey ( KeyCode.D ) ) {
+			transform.Rotate( -Vector3.forward * rotationSensitivity * Time.deltaTime );
+		}
+	}
+}
