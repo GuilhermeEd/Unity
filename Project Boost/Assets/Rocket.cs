8d6ef7e509579bf5c583dd1ca04e76fc2296e1b2
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-21 17:03:20 -0200

    Invoke() As A Coroutine Warm-up

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -1,28 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
 	[SerializeField] float boosterPower = 20f;
 	[SerializeField] float rotationSensitivity = 100f;
 
-	Rigidbody rb;
+	Rigidbody rigidBody;
 	AudioSource audioSource;
 
+	enum State { Alive, Dying, Transcending }
+	State state = State.Alive;
+
 	void Start () {
-		rb = GetComponent<Rigidbody>();
-		audioSource = GetComponent<AudioSource>();
+		rigidBody = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	void Update () {
-		Thrust();
-		Rotate();
+		if ( state == State.Alive ) {
+			Thrust();
+			Rotate();
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if ( state != State.Alive ) return;
+		switch ( collision.gameObject.tag ) {
+			case "Friendly":
+				// do nothing
+				break;
+			case "End":
+				state = State.Transcending;
+				Invoke ("LoadNextScene", 1f);
+				break;
+			default:
+				state = State.Dying;
+				Invoke ("LoadFirstScene", 1f);
+				break;
+		}
+	}
+
+  void LoadNextScene () {
+    int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    SceneManager.LoadScene(nextScene);
+  }
+
+	void LoadFirstScene () {
+		SceneManager.LoadScene (0);
 	}
 
   void Thrust () {
     if (Input.GetKey(KeyCode.Space)) {
-      rb.AddRelativeForce(Vector3.up * boosterPower);
+      rigidBody.AddRelativeForce(Vector3.up * boosterPower);
       if (!audioSource.isPlaying) audioSource.Play();
     } else audioSource.Stop();
 
@@ -31,11 +61,12 @@ public class Rocket : MonoBehaviour {
 
 	void Rotate () {
 		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
-			rb.freezeRotation = true; // take manual control of rotation
+			rigidBody.freezeRotation = true; // take manual control of rotation
 			Vector3 rotation = Vector3.forward * rotationSensitivity * Time.deltaTime;
 			if (Input.GetKey(KeyCode.A)) transform.Rotate(rotation);
 			else if (Input.GetKey(KeyCode.D)) transform.Rotate(-rotation);
-			rb.freezeRotation = false; // resume physics control of rotation
+			rigidBody.freezeRotation = false; // resume physics control of rotation
 		}
 	}
+
 }
