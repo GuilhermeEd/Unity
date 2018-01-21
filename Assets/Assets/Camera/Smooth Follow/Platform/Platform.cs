12f403f38b31d19e567e484afd37d1acd79b512d
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-20 23:22:26 -0200

    Added Assets Project

---
+++ Assets/Assets/Camera/Smooth Follow/Platform/Platform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public float range = 1.0f;
+    public float duration = 3.0f;
+
+    private float startY;
+    private float startTime;
+		private bool isMovingUp;
+
+    void Start() {
+			startY = transform.position.y;
+			startTime = Time.time;
+			isMovingUp = true;
+    }
+    void LateUpdate() {
+        float t = (Time.time - startTime) / duration;
+				if ( isMovingUp ) {
+        	transform.position = new Vector3(0f, Mathf.SmoothStep(startY, startY + range, t), 0f);
+					if ( transform.position.y >= startY + range ) {
+						isMovingUp = false;
+						startTime = Time.time;
+					}
+				} else {
+					transform.position = new Vector3(0f, Mathf.SmoothStep(startY + range, startY, t), 0f);
+					if ( transform.position.y <= startY ) {
+						isMovingUp = true;
+						startTime = Time.time;
+					}
+				}
+    }
+
+}
---
+++ Assets/Assets/Camera/Smooth Follow/Player/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float acceleration = 20f;
+	public float jumpPower = 400f;
+	public float maxSpeed = 100f;
+
+	private Rigidbody rb;
+	private bool isJumping;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		isJumping = false;
+	}
+	
+	void FixedUpdate () {
+		// Handle horizontal movement
+		if( Input.GetKey( KeyCode.RightArrow ) || Input.GetKey(KeyCode.D)){
+			if( Mathf.Abs ( rb.velocity.x ) < maxSpeed )	{
+				rb.AddForce( Vector3.right * acceleration * Time.deltaTime );
+			}
+		}
+		if( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey(KeyCode.A) ){
+			if( Mathf.Abs ( rb.velocity.x ) < maxSpeed )	{
+				rb.AddForce( Vector3.left * acceleration * Time.deltaTime );
+			}
+		}
+
+		// Handle jumps
+		if( ( Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) ){
+			if ( !isJumping ) {
+				rb.AddForce( Vector3.up * jumpPower * Time.deltaTime );
+			}
+		}
+
+		// Detect jump
+		float offset = (GetComponent<Collider>().bounds.size.y / 2) - 0.1f;
+		Vector3 origin = transform.position - Vector3.up * offset;
+		bool isHittingGround = Physics.Raycast(origin, Vector3.down, 0.2f);
+		isJumping = !isHittingGround;
+	}
+}
