f47ea8d13f00e79029cb4ba0defb35e74340df5a
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-27 17:55:22 -0200

    Added SmoothCameraFollow Script

---
+++ Assets/Assets/Camera/Smooth Camera Follow/SmoothCameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class SmoothCameraFollow : MonoBehaviour {
+
+	[SerializeField] Transform target;
+	[SerializeField] float smoothSpeed = 10f;
+	[SerializeField] Vector3 offset = new Vector3(0, 2, -10);
+
+	void Start () {
+		if ( !target ) {
+			target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+	}
+
+	void FixedUpdate () {
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothPosition;
+		transform.LookAt(target);
+	}	
+}
