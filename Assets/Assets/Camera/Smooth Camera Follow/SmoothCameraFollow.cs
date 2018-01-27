ffb12cb88a8ac16d6c24fb81facc90b02aba01c8
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-27 19:03:05 -0200

    Smooth Camera Follow Fix - Now uses Vector3.SmoothDamp

--- Assets/Assets/Camera/Smooth Camera Follow/SmoothCameraFollow.cs
+++ Assets/Assets/Camera/Smooth Camera Follow/SmoothCameraFollow.cs
@@ -7,6 +7,8 @@ public class SmoothCameraFollow : MonoBehaviour {
 	[SerializeField] float smoothSpeed = 10f;
 	[SerializeField] Vector3 offset = new Vector3(0, 2, -10);
 
+	Vector3 velocity = Vector3.zero;
+
 	void Start () {
 		if ( !target ) {
 			target = GameObject.FindGameObjectWithTag("Player").transform;
@@ -15,7 +17,7 @@ public class SmoothCameraFollow : MonoBehaviour {
 
 	void FixedUpdate () {
 		Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed * Time.fixedDeltaTime);
 		transform.position = smoothPosition;
 		transform.LookAt(target);
 	}	
