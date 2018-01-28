488e0cffa98d82c4b287ee0f01fb34770565aef8
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-28 14:02:00 -0200

    Added Ship Rotation with Position and Throw

--- Argon Assault/Assets/Player.cs
+++ Argon Assault/Assets/Player.cs
@@ -3,29 +3,55 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	[Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10f;
-	[Tooltip("In ms^-1")] [SerializeField] float ySpeed = 10f;
+	[Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+	[Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f;
 	[Tooltip("In m")] [SerializeField] float xRange = 5f;
 	[Tooltip("In m")] [SerializeField] float yRange = 3f;
+	[SerializeField] float positionPitchFactor = -5f;
+	[SerializeField] float controlPitchFactor = -20f;
+	[SerializeField] float positionYawFactor = 5f;
+	[SerializeField] float controlRollFactor = -20f;
+
+	float xThrow, yThrow;
 
 	void Start () {
 		
 	}
 	
-	void Update () {
-		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+	void Update ()
+  {
+    ProcessTranslation ();
+		ProcessRotation ();
+  }
 
-		float xOffset = xThrow * xSpeed * Time.deltaTime;
-		float yOffset = yThrow * ySpeed * Time.deltaTime;
+  void ProcessTranslation () {
+    xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+    yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
-		float rawNewX = transform.localPosition.x + xOffset;
-		float rawNewY = transform.localPosition.y + yOffset;
+    float xOffset = xThrow * xSpeed * Time.deltaTime;
+    float yOffset = yThrow * ySpeed * Time.deltaTime;
 
-		float clampedNewX = Mathf.Clamp(rawNewX, -xRange, xRange);
-		float clampedNewY = Mathf.Clamp(rawNewY, -yRange, yRange);
+    float rawNewX = transform.localPosition.x + xOffset;
+    float rawNewY = transform.localPosition.y + yOffset;
 
-		transform.localPosition = new Vector3(clampedNewX, clampedNewY, transform.localPosition.z);
+    float clampedNewX = Mathf.Clamp(rawNewX, -xRange, xRange);
+    float clampedNewY = Mathf.Clamp(rawNewY, -yRange, yRange);
+
+    transform.localPosition = new Vector3(clampedNewX, clampedNewY, transform.localPosition.z);
+  }
+
+	void ProcessRotation () {
+		float positionPitch = transform.localPosition.y * positionPitchFactor;
+		float controlPitch = yThrow * controlPitchFactor;
+		float pitch = positionPitch + controlPitch;
+		
+		float positionYaw = transform.localPosition.x * positionYawFactor;
+		float yaw = positionYaw;
+
+		float controlRoll = xThrow * controlRollFactor;
+		float roll = controlRoll;
+
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
 	}
 
 }
