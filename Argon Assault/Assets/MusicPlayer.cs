3491f26badb3cad9d0a2757e98538bb90f6aab08
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-28 11:14:38 -0200

    Added Circuit

---
+++ Argon Assault/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static GameObject singleton;
+
+	void Awake () {
+		if ( singleton ) {
+			Destroy( gameObject );
+		} else {
+			singleton = gameObject;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	void Start () {
+		Invoke("LoadFirstScene", 2f);
+	}
+
+	void LoadFirstScene () {
+		SceneManager.LoadScene(1);
+	}
+}
