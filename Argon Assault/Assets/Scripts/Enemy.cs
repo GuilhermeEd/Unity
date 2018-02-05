08c819a198761434c1d30a4ded638d32198f06c9
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-02-05 18:40:24 -0200

    Instantiating at Runtime

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 //[RequireComponent(typeof(BoxCollider))]
 public class Enemy : MonoBehaviour {
 
+	[SerializeField] GameObject deathFX;
+	[SerializeField] Transform parent;
+
 	void Start () {
 		BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
 		boxCollider.isTrigger = false;
 	}
 
 	void OnParticleCollision ( GameObject other ) {
+		GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+		fx.transform.parent = parent;
 		Destroy(gameObject);
 	}
 }
---
+++ Argon Assault/Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour {
+
+	ParticleSystem particles;
+
+	void Start () {
+		particles = GetComponent<ParticleSystem>();
+		float delay = particles.main.duration;
+		Destroy( gameObject, delay );
+	}
+
+}
