d54b63fd9173016d7f6e6126b0a3637cc3006c52
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-02-05 18:21:10 -0200

    Making Scripts Add Components

--- Argon Assault/Assets/Enemy.cs
+++ Argon Assault/Assets/Enemy.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(BoxCollider))]
 public class Enemy : MonoBehaviour {
 
-	void OnParticleCollision(GameObject other) {
+	void Start () {
+		BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = false;
+	}
+
+	void OnParticleCollision ( GameObject other ) {
 		Destroy(gameObject);
 	}
 }
